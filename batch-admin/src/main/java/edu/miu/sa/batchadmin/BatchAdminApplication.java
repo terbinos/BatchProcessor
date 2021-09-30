61587bbd21d8fc6ac9f6697770321b98565e71b8
Yordanos Desta Tegegn <yordan.desta@gmail.com>
2021-09-30 08:52:49 -0500

    initial commit

---
+++ batch-admin/src/main/java/edu/miu/sa/batchadmin/BatchAdminApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.sa.batchadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchAdminApplication.class, args);
+	}
+
+}
